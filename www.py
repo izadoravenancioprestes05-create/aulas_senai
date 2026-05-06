6db681a36e056ea387d55ff8a2e513648cc29703
izadoravenancioprestes05-create <izadoravenancioprestes05@gmail.com>
2026-05-06 16:59:43 -0300

    Add files via upload

---
+++ www.py
@@ -0,0 +1,14 @@
+import random 
+print("== Adivinhe o número === \n")
+secreto = random.randint(a=1, b=100)
+tentativas = 0
+palpite = 0
+while palpite != secreto:
+    palpite = int(input("Seu palpite (1-100): "))
+    tentativas += 1
+    if palpite < secreto:
+        print("Muito baixo!")
+    elif palpite > secreto:
+        print("Muito alto!")
+    else:
+        print(f"Parabéns! Você acertou em {tentativas} tentativas.")
